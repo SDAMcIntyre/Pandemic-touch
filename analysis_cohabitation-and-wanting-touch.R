4c270c88135bb52a3fcfd57e6fb773b8fbf64f81
S McIntyre <sdamcintyre+git@gmail.com>
2021-06-16 20:58:06 +0200

    proportion figures for wanted touch, added chi sq, added had touch

--- analysis_cohabitation-and-wanting-touch.R
+++ analysis_cohabitation-and-wanting-touch.R
@@ -2,8 +2,10 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(patchwork)
+# library(patchwork)
 library(svglite)
+# library(knitr)
+library(janitor)
 source("reorder_ordinals.R")
 source("plot_functions.R")
 
@@ -21,12 +23,53 @@ pandemic.data %>%
   pivot_longer(cols = starts_with('Wanted Touch'),  names_prefix = 'Wanted Touch ',
                names_to = 'Wanted Touch From', values_to = 'Response') %>% 
   na.omit() %>%
-  ggplot(aes(x = Response)) +
-  facet_grid(`Number Cohabiting` ~ `Wanted Touch From`, scales = 'free') +
-  geom_histogram(stat = 'count') +
+  ggplot() +
+  facet_grid(`Number Cohabiting` ~ `Wanted Touch From`) +
+  geom_bar(aes(x = Response, y = ..prop.., group = 1), stat = 'count') +
   theme_bw(base_size = 14) +
-  labs(y = 'Count', x = NULL, title = 'Wanted touch in the past week') +
-  theme(axis.text.x=element_text(angle=45, hjust = 1))
+  labs(y = 'Proportion', x = NULL, title = 'Wanted touch in the past week') +
+  theme(axis.text.x=element_text(angle=45, hjust = 1)) 
 
 ggsave('Figures/cohabitation-and-wanting-touch.svg')
 ggsave('Figures/cohabitation-and-wanting-touch.png')
+
+
+alone.data <- pandemic.data %>% 
+  filter(!is.na(`Number Cohabiting`)) %>% 
+  mutate(`Lives Alone` = if_else(`Number Cohabiting` == 'I live alone', TRUE, FALSE) ) %>% 
+  select(starts_with(c('Lives Alone', 'Wanted Touch'))) 
+
+# Pearson's chi-square test of equal distributions
+chisq.test(alone.data$`Lives Alone`, alone.data$`Wanted Touch Someone Close`)
+chisq.test(alone.data$`Lives Alone`, alone.data$`Wanted Touch Professional`)
+chisq.test(alone.data$`Lives Alone`, alone.data$`Wanted Touch Stranger`)
+
+# table with counts and percentages
+alone.data %>% 
+  tabyl(`Wanted Touch Someone Close`, `Lives Alone`, show_na = FALSE) %>%
+  adorn_totals('row') %>% 
+  adorn_percentages('col') %>% 
+  adorn_pct_formatting() %>% 
+  adorn_ns()
+
+#### Cohabiting & had touch ####
+
+quartz(11,7); plot(1:10)
+
+pandemic.data %>% 
+  select(starts_with(c('Number Cohabiting', 'Had Touch'))) %>% 
+  mutate(tempID = sample(1:n(), n())) %>% 
+  pivot_longer(cols = starts_with('Had Touch'),  names_prefix = 'Had Touch ',
+               names_to = 'Had Touch From', values_to = 'Response') %>% 
+  na.omit() %>%
+  ggplot() +
+  facet_grid(`Number Cohabiting` ~ `Had Touch From`) +
+  geom_bar(aes(x = Response, y = ..prop.., group = 1), stat = 'count') +
+  theme_bw(base_size = 14) +
+  labs(y = 'Proportion', x = NULL, title = 'Had touch in the past week') +
+  theme(axis.text.x=element_text(angle=45, hjust = 1)) 
+
+ggsave('Figures/cohabitation-and-had-touch.svg')
+ggsave('Figures/cohabitation-and-had-touch.png')
+
+
--- reorder_ordinals.R
+++ reorder_ordinals.R
@@ -6,7 +6,7 @@ reorder_ordinals <- function(df) {
       `Number Cohabiting` = factor(`Number Cohabiting`, levels = c('I live alone', 
                                                                    '1', '2', '3', 
                                                                    'More than 3')),
-      across(.cols = starts_with('Wanted Touch'),
+      across(.cols = starts_with(c('Wanted Touch', 'Had Touch')),
              .fns =  ~ factor(.x, levels = c('Not at all', 
                                              'Once', 
                                              '2-5 times',
